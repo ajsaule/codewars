6e6199238eca0bb9b3e4249bef8ef38668d9b681
andrej <andrejsaule8@gmail.com>
2020-09-23 12:22:11 +1000

    first PHP challenge completed

---
+++ substitute_vars_into_strings.php
@@ -0,0 +1,32 @@
+<?php
+# the lessons: 
+# - using online IDE's to get a faster feedback loop going seems buggy
+# - using string concatenation (.)operator to include expressions and functions within a return value 
+# - using double quotes "{$cake_pudding}" and curlies to interpolate variables into strings
+# - add <?php to start of file to get syntax highlighting for PHP 
+
+function solution($value){
+	$int_len = strlen($value."");
+	$zero_len = 5 - $int_len;
+	return "Value is ".str_repeat('0', $zero_len)."{$value}";
+};
+
+solution(5);
+
+
+# performing conole log into console with PHP we can do the following 
+# function console_log($data){
+#     echo '<scropt>';
+#     echo 'console.log('. json_encode($data) .')';
+#     echo '</script>';
+# };
+
+
+# Strange issue: 
+# - when running code with // commented lines, I get 'Parse error: syntax error, enexpected end of file'
+# - why would the comments be causing this ? 
+# - maybe change the comment style for PHP in package.json config file in VS code, but how to get to that file? 
+# - https://stackoverflow.com/questions/34822552/how-to-customize-comment-block-characters-in-visual-studio-code
+
+?>
+
